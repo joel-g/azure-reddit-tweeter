6072f47d6a0bde8e30910e83904dc65c098bb4c5
Joel Guerra <joelaguerra1@gmail.com>
2018-03-14 18:44:19 -0700

    adds reddit auth

--- azr.py
+++ azr.py
@@ -1,14 +1,33 @@
 import tweepy, praw
 
-config = open('config.ini','r')
-tokens = config.readlines()
-config.close()
+def authenticate_twitter():
+  config = open('config.ini','r')
+  tokens = config.readlines()
+  config.close()
+  CONSUMER_KEY = tokens[0].rstrip()
+  CONSUMER_SECRET = tokens[1].rstrip()
+  ACCESS_KEY = tokens[2].rstrip()
+  ACCESS_SECRET = tokens[3].rstrip()
+  print('Authenticating twitter...')
+  auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+  auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+  twitter = tweepy.API(auth)
+  print('Twitter authenticated.')
+  return twitter
 
-CONSUMER_KEY = tokens[0].rstrip()
-CONSUMER_SECRET = tokens[1].rstrip()
-ACCESS_KEY = tokens[2].rstrip()
-ACCESS_SECRET = tokens[3].rstrip()
+def authenticate_reddit():
+    print('Authenticating reddit...\n')
+    reddit = praw.Reddit('animal-facts-bot', user_agent='/u/AnimalFactsBot')
+    print('Reddit authenticated.')
+    return reddit
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-api = tweepy.API(auth)
+
+
+
+
+def main():
+  reddit = authenticate_reddit()
+  twitter = authenticate_twitter()
+
+if __name__ == '__main__':
+  main()
