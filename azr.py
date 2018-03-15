6b8044fc16092c785253d1265531c6c6ab22306e
Joel Guerra <joelaguerra1@gmail.com>
2018-03-14 20:00:58 -0700

    working bot

--- azr.py
+++ azr.py
@@ -1,50 +1,77 @@
-import tweepy, praw
+import tweepy, praw, time
+  
+with open('config.ini','r') as config:
+  tokens = config.readlines()
+  TW_CONSUMER_KEY = tokens[0].rstrip()
+  TW_CONSUMER_SECRET = tokens[1].rstrip()
+  TW_ACCESS_KEY = tokens[2].rstrip()
+  TW_ACCESS_SECRET = tokens[3].rstrip()
+  REDDIT_APP = tokens[4].rstrip()
+  REDDIT_USER = tokens[5].rstrip()
 
 def authenticate_twitter():
-  config = open('config.ini','r')
-  tokens = config.readlines()
-  config.close()
-  CONSUMER_KEY = tokens[0].rstrip()
-  CONSUMER_SECRET = tokens[1].rstrip()
-  ACCESS_KEY = tokens[2].rstrip()
-  ACCESS_SECRET = tokens[3].rstrip()
   print('Authenticating twitter...')
-  auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-  auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+  auth = tweepy.OAuthHandler(TW_CONSUMER_KEY, TW_CONSUMER_SECRET)
+  auth.set_access_token(TW_ACCESS_KEY, TW_ACCESS_SECRET)
   twitter = tweepy.API(auth)
   print('Twitter authenticated.')
   return twitter
 
 def authenticate_reddit():
     print('Authenticating reddit...\n')
-    reddit = praw.Reddit('animal-facts-bot', user_agent='/u/AnimalFactsBot')
+    reddit = praw.Reddit(REDDIT_APP, user_agent=REDDIT_USER)
     print('Reddit authenticated.')
     return reddit
 
 def get_reddit_posts(reddit):
-  posts = reddit.subreddit('azure').hot(limit=12)
-  # print(posts)
-  for post in posts:
-    print(post.title)
-    print(post.url)
-    print(post.id)
+  print("Fetching new posts...")
+  time.sleep(1)
+  posts = []
+  for post in reddit.subreddit('azure').hot(limit=12):
+    if "reddit.com" in post.url:
+      posts.append(post)
+  print("Returning " + str(posts.length) + " reddit posts")
+  return posts
+    
 
 def record_already_tweeted(submission_id):
+  print("Logging tweet...")
   writeable = open("tweeted.txt", 'a+')
   writeable.write(submission_id + '\n')
   writeable.close()
-
+  time.sleep(2)
+  
 def is_tweeted(submission_id):
+  print("Checking to see if this has already been tweeted...")
+  time.sleep(1)
   readable = open("tweeted.txt", "r")
   if submission_id in readable.read().splitlines():
+    print("It has been tweeted.")
+    time.sleep(1)
     return True
   else:
+    print("It has not been tweeted.")
+    time.sleep(1)
     return False
+
+def tweet(twitter, submission):
+  print("Tweeting...")
+  twitter.update_status(submission.title + " " + submission.url)
+  record_already_tweeted(submission.id)
+  time.sleep(2)
+  print("Tweeted!")
 
 def main():
   reddit = authenticate_reddit()
   twitter = authenticate_twitter()
-  get_reddit_posts(reddit)
+  while True:
+    for post in get_reddit_posts(reddit):
+      if not is_tweeted(post.id):
+        tweet(twitter, post)
+        print("Sleeping...")
+        time.sleep(10800)
+        break
+
 
 if __name__ == '__main__':
   main()
